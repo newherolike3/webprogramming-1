f8f81a36ea941f14baeb9fa4f0eb2e127bedbd0e
Morakot Thongprom <tonmorakot@gmail.com>
2021-06-21 11:29:44 +0700

    Spread + Destrucring

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-let x = 10;
+// Destructuring
 
-function show() {
-    let x = 20;
-    return x;
+let obj = {
+    name: "Morakot",
+    lastname: "Thongprom",
+    age: 36,
+    salary: 20000,
 }
 
-console.log(x);
-console.log(show());
-console.log(x);
+let obj2 = {
+    name: "Anothai",
+    lastname: "Jaidee",
+    age: 33,
+    salary: 25000,
+}
+
+const showdata = ({name, lastname, age, salary}) => ` สวัสดีคุณ ${name} ${lastname}
+ ขณะนี้คุณมีอายุ ${age}
+ เงินเดือนของคุณเท่ากับ ${salary}
+`;
+
+console.log(showdata(obj));
+console.log(showdata(obj2));
+
+
+// Spread Operator
+
+const fun = (x, y, z) => x + y + z;
+
+let num = [5, 6, 7];
+
+let res = fun(...num); //...num   fun(5, 6, 7);
+
+console.log(res);
