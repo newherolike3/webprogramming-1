498db546b7f35ab5fbf0f93595f6897e1b01c3a2
Morakot Thongprom <tonmorakot@gmail.com>
2021-06-21 10:19:55 +0700

    ตัวแปรแบบต่างๆ

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-let show = "RMUTL";
-let name = "Morakot Thongprom";
+// Morakot Thongprom
+/* 
+    week 2 - javascript
+*/
+let name = "Morakot";
+let lastname; // undefined
+lastname = "Thongprom";
 
-console.log("Javascript");
-console.log(show);
-console.log(name);
+// console.log(`สวัสดีคุณ ${name} ${lastname}`);
 
-// document.getElementById("show").innerHTML = show;
+const PI = 3.14;
+
+let obj = {
+    name: "Morakot",
+    lastname: "Thongprom",
+    address: {
+        number: "18/6",
+        province: "พิษณุโลก"
+    }
+}
+
+console.log(obj.name);
+console.log(obj.lastname);
+console.log(obj.address.number);
+console.log(obj.address.province);
