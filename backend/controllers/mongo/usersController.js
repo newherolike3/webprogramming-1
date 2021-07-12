4312441c551e335f5f39362a6ba5c2f0f2f8a718
Morakot Thongprom <tonmorakot@gmail.com>
2021-07-12 11:27:28 +0700

    Backend Week 5 อ่านและเพิ่มข้อมูล users

--- backend/controllers/mongo/usersController.js
+++ backend/controllers/mongo/usersController.js
@@ -1,9 +1,20 @@
 const users = require("../../models/mongo/users");
+// CRUD
 
+// R-Read อ่านข้อมูล
 exports.index = async (req, res, next) => {
-
     let data = await users.find();
-    
     res.status(200).json(data);
- 
+};
+
+// C-Create เขียนข้อมูล เพิ่มข้อมูล
+exports.insert = async (req, res, next) => {
+    let data = new users({
+        username: req.body.username,
+        email: req.body.email
+    });
+    data.save();
+    res.status(200).json({
+        message: "บันทึกข้อมูลเรียบร้อยแล้ว"
+    });
 };
