d6e0ceab53a61e220fb9e3cca9a0e0feecf49362
Morakot Thongprom <tonmorakot@gmail.com>
2021-09-13 09:36:31 +0700

    del

--- frontend/src/views/Customer.vue
+++ frontend/src/views/Customer.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-      <h1>
-          Customer
-      </h1>
+
+      <HelloWorld msg="ลูกค้า (VIP)" name="John" lastname="wick" />
+
+      <HelloWorld msg="ลูกค้า (ระดับ 1)" name="aaaa" lastname="1111" />
+
+      <HelloWorld msg="ลูกค้า (ระดับ 2)" name="bbbb" lastname="2222" />
+
+      <HelloWorld msg="ลูกค้า (ระดับ 3)" name="cccc" lastname="3333" />
+
   </div>
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
 export default {
+
+  components: {
+    HelloWorld
+  }
 
 }
 </script>
